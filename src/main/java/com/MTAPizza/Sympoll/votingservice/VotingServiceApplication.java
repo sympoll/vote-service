3dda9093b18c85d0e70109de4494a2b5d0ab76cd
Ronenii <ronen.glm@gmail.com>
2024-04-22 15:25:17 +0300

    -Initialized repo

---
+++ src/main/java/com/MTAPizza/Sympoll/votingservice/VotingServiceApplication.java
@@ -0,0 +1,12 @@
+package com.MTAPizza.Sympoll.votingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/MTAPizza/Sympoll/votingservice/VotingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MTAPizza.Sympoll.votingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
