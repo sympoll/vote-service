51d49d0874cb233cc7f6a35781b2c48542cfad7a
Ronenii <ronen.glm@gmail.com>
2024-08-08 16:21:16 +0300

    - Added base for testing

---
+++ src/main/java/com/MTAPizza/Sympoll/votingservice/validator/exception/VoteExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.MTAPizza.Sympoll.votingservice.validator.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class VoteExceptionHandler {
+
+    // TODO: Implement this.
+}
--- src/test/java/com/MTAPizza/Sympoll/votingservice/VotingServiceApplicationTests.java
+++ src/test/java/com/MTAPizza/Sympoll/votingservice/VotingServiceApplicationTests.java
@@ -1,13 +1,47 @@
 package com.MTAPizza.Sympoll.votingservice;
 
-import org.junit.jupiter.api.Test;
+import com.MTAPizza.Sympoll.votingservice.validator.exception.VoteExceptionHandler;
+import com.google.gson.Gson;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@Import(VoteExceptionHandler.class)
 class VotingServiceApplicationTests {
+    private static final Gson gson;
 
-	@Test
-	void contextLoads() {
-	}
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgreSQLContainer =
+            new PostgreSQLContainer<>("postgres:16.2").withInitScript("init.sql");
+
+    @LocalServerPort
+    private Integer port;
+
+    @BeforeEach
+    void setup() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
+
+    static {
+        postgreSQLContainer.start(); //  Run mock test container.
+        gson = new Gson();
+    }
+
+    @Test
+    @Order(1)
+    void shouldCastVote(){
+
+    }
 
 }
