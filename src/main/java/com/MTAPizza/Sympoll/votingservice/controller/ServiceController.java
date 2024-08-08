33529d60b59364e825f947a991bac10e254b8357
Ronenii <ronen.glm@gmail.com>
2024-08-08 16:40:39 +0300

    - Added todo endpoints

--- src/main/java/com/MTAPizza/Sympoll/votingservice/controller/ServiceController.java
+++ src/main/java/com/MTAPizza/Sympoll/votingservice/controller/ServiceController.java
@@ -1,4 +1,7 @@
 package com.MTAPizza.Sympoll.votingservice.controller;
 
 public class ServiceController {
+    // TODO: implement POST endpoint /api/vote. Create new vote.
+    // TODO: implement DELETE endpoint /api/vote. Delete a vote (By user_id and voting_item_id).
+    // TODO: implement GET endpoint /api/vote/count. Returns number of votes for specified poll.
 }
