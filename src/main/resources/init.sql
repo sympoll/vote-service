b812a53509dbf4d014fc7a9a19d08f05ab0f1efb
Ronenii <ronen.glm@gmail.com>
2024-08-15 14:03:24 +0300

    - removed init.sql from resources

--- src/main/resources/init.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE votes
-(
-    vote_id        UUID PRIMARY KEY,
-    user_id        UUID NOT NULL,
-    voting_item_id INT  NOT NULL,
-    vote_datetime  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
