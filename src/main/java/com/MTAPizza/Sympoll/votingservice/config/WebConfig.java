a4570f1e6211987db1a225bf86a14a6dc8a369d7
Ronenii <ronen.glm@gmail.com>
2024-08-29 13:32:15 +0300

    Added origin

--- src/main/java/com/MTAPizza/Sympoll/votingservice/config/WebConfig.java
+++ src/main/java/com/MTAPizza/Sympoll/votingservice/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080")  // Replace '*' with the specific origin
+                .allowedOrigins("http://localhost:8080", "http://localhost")
                 .allowCredentials(true)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
